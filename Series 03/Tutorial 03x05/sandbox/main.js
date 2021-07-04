d65bb9470f7f38d1eab630e7cfb0361c2cca2840
julia4ta <70852660+julia4ta@users.noreply.github.com>
2021-07-03 23:21:43 -0600

    Add files via upload

---
+++ Series 03/Tutorial 03x05/sandbox/main.js
@@ -0,0 +1,15 @@
+// document.getElementById("sandbox").innerHTML = "Hello, World!";
+
+// let dog = {
+//     fName: "Dabbling",
+//     lName: "Doggo",
+//     fullName: function () {
+//         return this.fName + " " + this.lName;
+//     }
+// }
+
+// document.getElementById("sandbox").innerHTML = dog.fullName();
+
+function hello() {
+    document.getElementById("sandbox").innerHTML = "Hello, World!";
+}
