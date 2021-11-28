41a208f966b0934d7275c236c89c5988ae9a2cf6
julia4ta <70852660+julia4ta@users.noreply.github.com>
2021-11-27 22:11:14 -0700

    Add files via upload

---
+++ Series 04/Tutorial 04x11/rungame.jl
@@ -0,0 +1,11 @@
+using GameZero
+
+# rungame("Simulations/particles.jl")
+
+# rungame("Simulations/bounce.jl")
+
+# rungame("Simulations/rotations.jl")
+
+# rungame("Simulations/sinewave.jl")
+
+rungame("Simulations/pendulum.jl")
