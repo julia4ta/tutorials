76437172a9d17737f4d18bdc08f1860e0b6f1e7b
doggodotjl <70852660+julia4ta@users.noreply.github.com>
2023-09-30 16:28:56 -0600

    Add files via upload

---
+++ Series 10/Tutorial 10x07/math.jl
@@ -0,0 +1,31 @@
+# this is a single-line comment
+
+# keyboard shortcut for comments is Ctrl+/
+
+#=
+    this is a
+    multi-line
+    comment
+=#
+
+# basic math
+
+1 + 1
+
+1+1
+
+2 - 1
+
+2 * 3
+
+4 / 2
+
+5 / 2
+
+2^3
+
+5 % 2
+
+# order of operations
+
+1 + (2 - 3)^4 * 5
