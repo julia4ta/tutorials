e92e6368a7f23c4e4cf548a8ab776149440beba7
doggodotjl <70852660+julia4ta@users.noreply.github.com>
2023-09-30 16:29:52 -0600

    Add files via upload

---
+++ Series 10/Tutorial 10x08/bool.jl
@@ -0,0 +1,21 @@
+# comparison operators
+
+3 < 2
+
+3 >= 3
+
+4 == 3
+
+# Boolean operators
+
+# && short-circuiting AND
+
+1 == 1 && 1 > 1
+
+# || short-circuiting OR
+
+1 == 1 || 1 > 1
+
+# true = 1 and false = 0
+
+false + true + true
