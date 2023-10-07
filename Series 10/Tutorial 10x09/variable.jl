d39b2e4333dabda91b40bc637723cf7073b0f77d
doggodotjl <70852660+julia4ta@users.noreply.github.com>
2023-10-07 07:30:14 -0600

    Add files via upload

---
+++ Series 10/Tutorial 10x09/variable.jl
@@ -0,0 +1,35 @@
+# variable
+
+x = 1
+
+y = 2
+
+z = x + y
+
+x = 2
+
+y
+
+z
+
+z = x + y
+
+# how to increment
+
+i = 1
+
+i = i + 1
+
+i += 1
+
+# how to perform basic math
+# using variables
+
+height = 2
+
+width = 3
+
+area = height * width
+
+# use Ctrl+Enter to evaluate
+# a single line of code
