80b5fa7e71e9cfa36fbbfa92837273023e7d0301
doggodotjl <70852660+julia4ta@users.noreply.github.com>
2023-10-14 12:05:57 -0600

    Add files via upload

---
+++ Series 10/Tutorial 10x10/numbers.jl
@@ -0,0 +1,47 @@
+# numbers
+
+typeof(-3)
+
+typeof(1.2)
+
+example::Float32 = 1.2
+
+typeof(example)
+
+0.1 + 0.2
+
+1 / 3
+
+typeof(1 / 3)
+
+1//3
+
+typeof(1//3)
+
+1//3 + 1//7
+
+pi
+
+typeof(pi)
+
+round(pi; digits = 5)
+
+1000000 == 1_000_000
+
+sqrt(4)
+
+# from chapter on basic math
+
+4 / 2
+
+5 / 2
+
+div(4, 2)
+
+div(5, 2)
+
+# \div<tab>
+
+4 ÷ 2
+
+5 ÷ 2
