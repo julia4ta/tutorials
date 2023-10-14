55348572028d47f6dd092cd66ed184518dc858a5
doggodotjl <70852660+julia4ta@users.noreply.github.com>
2023-10-14 12:06:50 -0600

    Add files via upload

---
+++ Series 10/Tutorial 10x11/strings.jl
@@ -0,0 +1,57 @@
+# characters and strings
+
+typeof('a')
+
+typeof("b")
+
+typeof("Hello, World!")
+
+# how to display quotation marks
+
+println("doggo says \"bork!\".")
+
+println("""doggo says "bork!".""")
+
+# how to display new lines
+
+println("1\n2\n3")
+
+# how to display tabs
+
+println("1\t2\t3")
+
+# how to concatenate
+
+s1 = "Hello, "
+
+s2 = "World!"
+
+s1_s2 = s1 * s2
+
+# how to interpolate
+
+s3 = "doggo"
+
+println("$s3 dot jl")
+
+# unicode characters
+
+# \alpha<tab>
+typeof('α')
+
+# \:dog:<tab>
+typeof('🐶')
+
+α = 1
+
+🐶 = 2
+
+α + 🐶
+
+# \pi<tab>
+π
+
+π == pi
+
+# \euler<tab>
+ℯ
