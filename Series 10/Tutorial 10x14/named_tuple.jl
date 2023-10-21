51969879a0f8dc19ac0df6f99ea40c2dcdeb4d93
doggodotjl <70852660+julia4ta@users.noreply.github.com>
2023-10-21 17:39:33 -0600

    Add files via upload

---
+++ Series 10/Tutorial 10x14/named_tuple.jl
@@ -0,0 +1,19 @@
+# named tuple
+
+# construct named tuple
+
+dog = (
+    name = "eggdog",
+    age = 3,
+    breed = "egg-dog mix"
+)
+
+typeof(dog)
+
+# access named tuple element
+
+dog[1]
+
+dog.name
+dog.age
+dog.breed
