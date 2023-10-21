f0f2983b1a405a6c227ea5dfe711419c473a6321
doggodotjl <70852660+julia4ta@users.noreply.github.com>
2023-10-21 17:38:47 -0600

    Add files via upload

---
+++ Series 10/Tutorial 10x13/tuple.jl
@@ -0,0 +1,31 @@
+# tuple
+
+# construct tuple
+
+dog = ("eggdog", 3, "egg-dog mix")
+
+typeof(dog)
+
+# access tuple element
+
+dog[1]
+
+# tuple elements are immutable
+
+# dog[1] = "doggo"
+
+# tuple element order is immutable
+
+# sort(dog)
+
+# tuple size is immutable
+
+# push!(dog, "doggo@doggo.doggo")
+
+# destructure assignment
+
+(name, age, breed) = dog
+
+name
+age
+breed
